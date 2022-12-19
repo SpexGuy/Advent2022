e4811bebc95de83eae993c0c449f5ea851aff268
Martin Wickham <spexguy070@gmail.com>
2022-12-19 01:31:44 -0600

    Day 18

--- src/day18.zig
+++ src/day18.zig
@@ -9,37 +9,159 @@ const util = @import("util.zig");
 const gpa = util.gpa;
 
 const data = @embedFile("data/day18.txt");
+const test_data =
+\\2,2,2
+\\1,2,2
+\\3,2,2
+\\2,1,2
+\\2,3,2
+\\2,2,1
+\\2,2,3
+\\2,2,4
+\\2,2,6
+\\1,2,5
+\\3,2,5
+\\2,1,5
+\\2,3,5
+;
+
+const use_data = data;
 
 const Item = struct {
     v: i64,
 
 };
 
+const Pt3 = struct{
+    x: usize, y: usize, z: usize
+};
+
 pub fn main() !void {
     var part1: i64 = 0;
     var part2: i64 = 0;
 
-    var items_list = List(Item).init(gpa);
-    var lines = tokenize(u8, data, "\n\r");
+    var max = Pt3{ .x = 0, .y = 0, .z = 0 };
+    var items_list = List(Pt3).init(gpa);
+    var lines = tokenize(u8, use_data, "\n\r");
     while (lines.next()) |line| {
-        var parts = split(u8, line, " ");
-        const v = parts.next().?;
-
+        var parts = split(u8, line, ",");
+        const pt = Pt3{
+            .x = (try parseInt(usize, parts.next().?, 10)) + 2,
+            .y = (try parseInt(usize, parts.next().?, 10)) + 2,
+            .z = (try parseInt(usize, parts.next().?, 10)) + 2,
+        };
         assert(parts.next() == null);
 
-        try items_list.append(.{
-            .v = try parseInt(i64, v, 10),
-        });
+        try items_list.append(pt);
+        max.x = max2(max.x, pt.x);
+        max.y = max2(max.y, pt.y);
+        max.z = max2(max.z, pt.z);
+    }
+
+    max.x += 3;
+    max.y += 3;
+    max.z += 3;
+
+    const ypitch = max.x + 1;
+    const zpitch = max.y * ypitch + 1;
+
+    const grid = try gpa.alloc(u8, zpitch * max.z);
+    std.mem.set(u8, grid, '.');
+    {
+        var z: usize = 0;
+        while(z < max.z) : (z += 1) {
+            const slice_base = z * zpitch;
+            grid[slice_base + zpitch - 1] = '\n';
+            var y: usize = 0;
+            while (y < max.y) : (y += 1) {
+                const rowbase = slice_base + y * ypitch;
+                grid[rowbase + ypitch - 1] = '\n';
+            }
+        }
     }
 
     const items = items_list.items;
 
-    // Do stuff
+
     for (items) |it| {
-        _ = &it;
+        const index = it.z * zpitch + it.y * ypitch + it.x;
+        grid[index] = '#';
     }
 
+    print("{s}\n", .{grid});
+
+    {
+    var z: usize = 0;
+    while (z < max.z - 1) : (z += 1) {
+    var y: usize = 0;
+    while (y < max.y - 1) : (y += 1) {
+    var x: usize = 0;
+    while (x < max.x - 1) : (x += 1) {
+        const index = z * zpitch + y * ypitch + x;
+        const base = grid[index];
+        if (base != grid[index + 1]) part1 += 1;
+        if (base != grid[index + ypitch]) part1 += 1;
+        if (base != grid[index + zpitch]) part1 += 1;
+    }
+    }
+    }
+    }
+
+    grid[0] = '1';
+
+    var progress = true;
+    while (progress) {
+        progress = false;
+        var z: usize = 0;
+        while (z < max.z - 1) : (z += 1) {
+        var y: usize = 0;
+        while (y < max.y - 1) : (y += 1) {
+        var x: usize = 0;
+        while (x < max.x - 1) : (x += 1) {
+            const index = z * zpitch + y * ypitch + x;
+            const base = grid[index];
+            if (base == '.') {
+                if (grid[index + 1] == '1' or
+                grid[index + ypitch] == '1' or
+                grid[index + zpitch] == '1') {
+                    grid[index] = '1';
+                    progress = true;
+                }
+            } else if (base == '1') {
+                if (grid[index + 1] == '.') { grid[index + 1] = '1'; progress = true; }
+                if (grid[index + ypitch] == '.') { grid[index + ypitch] = '1'; progress = true; }
+                if (grid[index + zpitch] == '.') { grid[index + zpitch] = '1'; progress = true; }
+            }
+        }
+        }
+        }
+    }
+
+    {
+    var z: usize = 0;
+    while (z < max.z - 1) : (z += 1) {
+    var y: usize = 0;
+    while (y < max.y - 1) : (y += 1) {
+    var x: usize = 0;
+    while (x < max.x - 1) : (x += 1) {
+        const index = z * zpitch + y * ypitch + x;
+        const base = grid[index];
+        if (part2Test(base, grid[index + 1])) part2 += 1;
+        if (part2Test(base, grid[index + ypitch])) part2 += 1;
+        if (part2Test(base, grid[index + zpitch])) part2 += 1;
+   }
+    }
+    }
+    }
+
+    print("{s}\n", .{grid});
+
     print("part1: {}\npart2: {}\n", .{part1, part2});
+}
+
+fn part2Test(a: u8, b: u8) bool {
+    return a == '1' and b == '#' or
+        a == '#' and b == '1';
 }
 
 // Useful stdlib functions
