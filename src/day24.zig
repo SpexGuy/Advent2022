8eca9f462dbe33937eb12e90bf6054e5348f2c76
Martin Wickham <spexguy070@gmail.com>
2022-12-23 23:50:33 -0600

    Day 24

--- src/day24.zig
+++ src/day24.zig
@@ -11,33 +11,107 @@ const gpa = util.gpa;
 const data = file_data;
 const file_data = @embedFile("data/day24.txt");
 const test_data =
+\\#.######
+\\#>>.<^<#
+\\#.<..<<#
+\\#>v.><>#
+\\#<^v^^>#
+\\######.#
 \\
 ;
 
-const Item = struct {
-    v: i64,
+fn pathfind(grid: *Grid, from: usize, to: usize) !usize {
+    var positions = std.AutoArrayHashMap(usize, void).init(gpa);
+    try positions.put(from, {});
 
-};
+    var iter: usize = 0;
+    while (true) : (iter += 1) {
+        // Make a new grid
+        const new_grid = try gpa.dupe(u8, grid.data);
+        defer {
+            gpa.free(grid.data);
+            grid.data = new_grid;
+        }
+
+        // Preserve only walls
+        for (new_grid) |*ch| ch.* &= 16;
+        // Update storms
+        for (grid.data[grid.offset..grid.data.len - grid.offset]) |ch, ni| {
+            const i = ni + grid.offset;
+            if (ch & 1 != 0) {
+                var next = i + 1;
+                if (grid.data[next] & 16 != 0) {
+                    next = next + 2 - grid.width; 
+                }
+                new_grid[next] |= 1;
+            }
+            if (ch & 2 != 0) {
+                var next = i + grid.pitch;
+                if (grid.data[next] & 16 != 0) {
+                    //print("down into wall: {}, {b}\n", .{grid.factor(next), ch});
+                    next = next - (grid.height - 2) * grid.pitch; 
+                }
+                new_grid[next] |= 2;
+            }
+            if (ch & 4 != 0) {
+                var next = i - 1;
+                if (grid.data[next] & 16 != 0) {
+                    next = next + grid.width - 2; 
+                }
+                new_grid[next] |= 4;
+            }
+            if (ch & 8 != 0) {
+                var next = i - grid.pitch;
+                if (grid.data[next] & 16 != 0) {
+                    next = next + (grid.height - 2) * grid.pitch; 
+                }
+                new_grid[next] |= 8;
+            }
+        }
+        // Update positions
+        var new_positions = std.AutoArrayHashMap(usize, void).init(gpa);
+        for (positions.keys()) |key| {
+            for ([_]usize{
+                key - grid.pitch,
+                key - 1,
+                key,
+                key + 1,
+                key + grid.pitch, 
+            }) |next| {
+                if (new_grid[next] == 0) {
+                    if (next == to) return iter + 1;
+                    try new_positions.put(next, {});
+                }
+            }
+        }
+
+        positions.deinit();
+        positions = new_positions;
+    }
+}
 
 pub fn main() !void {
-    var part1: i64 = 0;
-    var part2: i64 = 0;
+    var grid = try Grid.load(data, '#', 1);
 
-    var items_list = List(Item).init(gpa);
-    var lines = tokenize(u8, data, "\n\r");
-    while (lines.next()) |line| {
-        var parts = split(u8, line, " ");
-        try items_list.append(.{
-            .v = try parseInt(i64, parts.next().?, 10),
-        });
-        assert(parts.next() == null);
+    for (grid.data) |*ch| {
+        ch.* = switch (ch.*) {
+            '#' => 16,
+            '.' => 0,
+            '>' => 1,
+            'v' => 2,
+            '<' => 4,
+            '^' => 8,
+            '\n' => 16,
+            else => unreachable,
+        };
     }
-    const items = items_list.items;
 
-    // Do stuff
-    for (items) |it| {
-        _ = &it;
-    }
+    const start = grid.indexOf(1, 0);
+    const end = grid.indexOf(grid.width-2, grid.height-1);
+    const part1 = try pathfind(&grid, start, end);
+    var part2 = part1;
+    part2 += try pathfind(&grid, end, start);
+    part2 += try pathfind(&grid, start, end);
 
     print("part1: {}\npart2: {}\n", .{part1, part2});
 }
